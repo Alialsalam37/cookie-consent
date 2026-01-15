1fb21cdada39ab9f579b5ad71b046d968c079a9b
Alialsalam37 <alisalam65420@gmail.com>
2026-01-16 01:10:26 +0300

    update

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const modalCloseBtn = document.getElementById("modal-close-btn")
 const consentForm = document.getElementById("consent-form")
 const modalText = document.getElementById("modal-text")
 const modalInner = document.getElementById("modal-inner")
+const declineBtn = document.getElementById("declineBtn")
+const modalChoiceBtns = document.getElementById("modal-choice-btns")
 
 setTimeout(function () {
     modal.style.display = "inline"
@@ -12,7 +14,15 @@ modalCloseBtn.addEventListener('click', function () {
     modal.style.display = "none"
 })
 
+declineBtn.addEventListener('mouseenter', function () {
+    console.log("hover")
+    modalChoiceBtns.classList.toggle('reverse')
+})
+
 consentForm.addEventListener("submit", function (e) {
+    const loginForm = new FormData(consentForm)
+    const fullName = loginForm.get('fullName')
+
     e.preventDefault()
     modalText.innerHTML = `
     <div class="modal-inner-loading">
@@ -28,12 +38,13 @@ consentForm.addEventListener("submit", function (e) {
 
     setTimeout(function () {
         modalInner.innerHTML = `
-            <h2>Thanks you sucker! </h2>
+            <h2>Thanks ${fullName} sucker! </h2>
             <p>We just sold the rights to your eternal soul.</p>
             <div class="idiot-gif">
             <img src="images/pirate.gif">
             </div>
         `
+        modalCloseBtn.disabled = false
     }, 3000)
 
 })
